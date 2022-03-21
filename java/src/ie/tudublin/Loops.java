04ab806ac390061960caa476d49b08e2966f19fe
C20447296 Sean Lawless <c20447296@mytudublin.ie>
2022-03-21 11:18:53 +0000

    AUDIO

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -69,8 +69,67 @@ public class Loops extends PApplet {
 			case 1:
 			
 				break;
+<<<<<<< HEAD
 				//map(a,b,c,d,e);
 				//a = inputvalue
+=======
+			case 2:
+				background(255);
+				int circles = (int) (mouseX / 20.0f);
+				offset += (mouseY / 100.0f);
+				float d = width / (float) circles;
+				for (int j = 0; j < circles; j++) {
+					for (int i = 0; i < circles; i++) {
+						noStroke();
+						float c = map((i + j + offset), 0, circles * 2, 0, 255) % 256;
+						fill(c, 255, 255);
+						float x = map(i, 0, circles - 1, d / 2.0f, width - (d / 2.0f)); 
+						float y = map(j, 0, circles - 1, d / 2.0f, width - (d / 2.0f)); 
+						circle(x, y, d);
+					}
+				}
+				break;
+			case 3:
+				background(0);
+				colorMode(RGB);
+				float border = width * 0.1f;
+				for(int i = -5; i <= 5; i ++)
+				{
+					float x = map(i, -5, 5, border, width - border);
+					stroke(0, 255, 0);
+					line(x, border, x, height - border);
+					line(border, x, width - border, x);
+					fill(255);
+					text(i, x, border * 0.5f);
+					text(i, border * 0.5f, x);
+				}
+			case 4:
+				background(0);
+				stroke(255, 255, 255);	
+				float cx = width / 2;
+				float cy = height / 2;	
+				float radius = 200;		
+				int points = (int)map(mouseX, 1, width, 5, 20);
+				int sides = points * 2;
+				float px = cx;
+				float py = cy - radius; 
+				for(int i = 0 ; i <= sides ; i ++)
+				{
+					float r = (i % 2 == 0) ? radius : radius / 2; 
+					// float r = radius;
+					float theta = map(i, 0, sides, 0, TWO_PI);
+					float x = cx + sin(theta) * r;
+					float y = cy - cos(theta) * r;
+					
+					//circle(x, y, 20);
+					line(px, py, x, y);
+					px = x;
+					py = y;
+				}
+
+				// map(a,b,c,d,e);
+				// a = inputvalue
+>>>>>>> 0e4ccf8c75afbd2fba88b712508569ab5bba12ee
 				// b - c - start and end of the first range
 				// d, e 0 - start and and of the end range
 
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -36,6 +36,7 @@ public class Main
         processing.core.PApplet.runSketch( a, new Arrays());
     }
 
+<<<<<<< HEAD
     public static void BugZap()
 
 	{
@@ -44,6 +45,33 @@ public class Main
     }
 
 
+=======
+    public static void life()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Life());
+    }
+
+
+    public static void audio1()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio1());
+    }
+
+    public static void audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }
+
+    public static void colorfulLife()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new ColorfulLife());
+    }
+
+>>>>>>> 0e4ccf8c75afbd2fba88b712508569ab5bba12ee
     public void cat()
     {
         System.out.println("Hello world");
@@ -69,7 +97,11 @@ public class Main
     }
     public static void main(String[] args)
     {
+<<<<<<< HEAD
         Audio1();
+=======
+        audio2();
+>>>>>>> 0e4ccf8c75afbd2fba88b712508569ab5bba12ee
 
         // Tara Misty
         // Tara Tara
